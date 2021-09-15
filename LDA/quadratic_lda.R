8df50605e901629f148cad9c5bf1ff279589eb53
my-cabbages <ibaumann.randb@gmail.com>
2021-09-15 16:00:07 -0400

    initial commit

---
+++ LDA/quadratic_lda.R
@@ -0,0 +1,307 @@
+# Quadratic discriminant analysis in a multivariate setting with three classes
+
+# Load packages and read data
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, MASS)
+
+#####
+#. Step 1: Data generation
+
+# Generate sample data for three outcomes (0, 1, and 2)
+
+# We will use two predictors in this example, using MASS's `mvrnorm` function
+# to sample from a multivariate normal distribution. `mu` will be our vector of
+# population means for the predictors. Our predictors will be correlated with
+# one another, which will be reflected in the covariance matrix `sigma`
+
+# Choose population means per group, centered on (X1, X2, X3) coordinate means
+
+pop_mean_c0_X1 <- sample.int(10, 1)
+pop_mean_c1_X1 <- sample.int(10, 1)
+pop_mean_c2_X1 <- sample.int(10, 1)
+
+pop_mean_c0_X2 <- sample.int(10, 1)
+pop_mean_c1_X2 <- sample.int(10, 1)
+pop_mean_c2_X2 <- sample.int(10, 1)
+
+mu_c0 <- c(pop_mean_c0_X1, pop_mean_c0_X2)
+mu_c1 <- c(pop_mean_c1_X1, pop_mean_c1_X2)
+mu_c2 <- c(pop_mean_c2_X1, pop_mean_c2_X2)
+
+# Generate sample data: predictor variance will be on the diagonals and 
+# covariance on the immediate off-diagonals. Correlation is between variables
+# (X1 and X2) and variance is within-group and identical for both groups
+
+# Note that QDA *does not* assume each class has the same covariance matrix, so
+# we will create separate population covariance matrices for each pair of
+# variables
+
+pop_c0_corr <- runif(1, 0, 1)
+pop_c1_corr <- runif(1, 0, 1)
+pop_c2_corr <- runif(1, 0, 1)
+
+pop_var_c0 <- runif(1, 0, 10)
+pop_var_c1 <- runif(1, 0, 10)
+pop_var_c2 <- runif(1, 0, 10)
+
+# Generate sample data from multivariate normal distributions, choosing n samples
+
+n <- 300
+
+for (i in 0:2) {
+  
+  class <- paste("c", i, sep = "")
+  
+  class_var <- paste("pop_var", class, sep = "_")
+  class_corr <- paste("pop", class, "corr", sep = "_")
+  mu = paste("mu", class, sep = "_")
+  
+  temp_sigma <- matrix(c(get(class_var), get(class_corr),
+                        get(class_corr), get(class_var)), 2, 2)
+  
+  temp_train_data <- mvrnorm(n = n,
+                             mu = get(mu),
+                             Sigma = temp_sigma)
+  
+  temp_test_data <- mvrnorm(n = n,
+                            mu = get(mu),
+                            Sigma = temp_sigma)
+  
+  assign(paste(class, "sigma", sep = "_"), temp_sigma)
+  assign(paste(class, "train", sep = "_"), temp_train_data)
+  assign(paste(class, "test", sep = "_"), temp_test_data)
+  
+}
+
+train_sample_df <- bind_rows(
+  tibble(y = 0,
+         X1 = c0_train[,1],
+         X2 = c0_train[,2]),
+  tibble(y = 1,
+         X1 = c1_train[,1],
+         X2 = c1_train[,2]),
+  tibble(y = 2,
+         X1 = c2_train[,1],
+         X2 = c2_train[,2])
+)
+
+test_sample_df <- bind_rows(
+  tibble(y = 0,
+         X1 = c0_test[,1],
+         X2 = c0_test[,2]),
+  tibble(y = 1,
+         X1 = c1_test[,1],
+         X2 = c1_test[,2]),
+  tibble(y = 2,
+         X1 = c2_test[,1],
+         X2 = c2_test[,2])
+)
+
+# Plot 95% confidence ellipses for the groups
+
+train_conf_ellipse <- ggplot(train_sample_df) + 
+  stat_ellipse(aes(x = X1, y = X2, group = as.factor(y), color = as.factor(y))) +
+  scale_color_manual(values = c("red", "blue", "green"),
+                     name = "Class") +
+  labs(title = "Bivariate Training Sample of Three Classes: 95% Probability Ellipses")
+
+train_conf_ellipse
+
+test_conf_ellipse <- ggplot(test_sample_df) + 
+  stat_ellipse(aes(x = X1, y = X2, group = as.factor(y), color = as.factor(y))) +
+  scale_color_manual(values = c("red", "blue", "green"),
+                     name = "Class") +
+  labs(title = "Bivariate Test Sample of Three Classes: 95% Probability Ellipses")
+
+test_conf_ellipse
+
+# Plot scatterplot of data
+
+train_sample_scatter <- ggplot(train_sample_df) + 
+  geom_point(aes(x = X1, y = X2, color = as.factor(y))) +
+  scale_color_manual(values = c("red", "blue", "green"),
+                     name = "Class") +
+  labs(title = "Bivariate Training Sample of Three Classes: Scatterplot")
+
+train_sample_scatter
+
+test_sample_scatter <- ggplot(test_sample_df) + 
+  geom_point(aes(x = X1, y = X2, color = as.factor(y))) +
+  scale_color_manual(values = c("red", "blue", "green"),
+                     name = "Class") +
+  labs(title = "Bivariate Test Sample of Three Classes: Scatterplot")
+
+test_sample_scatter
+
+#####
+# Step 2: Calculate optimal Bayes decision boundary (for comparison)
+
+# As in the univariate case, we will build the Bayes classifier for comparison,
+# but in this case the classifier is more complex and more closely resembles the
+# construction of the univariate LDA classifier (after all, LDA is essentially
+# an approximation of the Bayes classifier).
+
+# We will calculate the class-score for each observation (note that this is
+# likely not a stable or efficient way to do this in a finite-precision
+# computational setting)
+
+pop_c0_mean_vec <- c(pop_mean_c0_X1, pop_mean_c0_X2)
+pop_c1_mean_vec <- c(pop_mean_c1_X1, pop_mean_c1_X2)
+pop_c2_mean_vec <- c(pop_mean_c2_X1, pop_mean_c2_X2)
+
+c0_prior <- n / (n * 3)
+c1_prior <- n / (n * 3)
+c2_prior <- n / (n * 3)
+
+d0_bayes <- function(x_vec){
+  -.5 * log(norm(c0_sigma, type = "2")) - 
+    .5 * t(x_vec - mu_c0) %*% solve(c0_sigma) %*% (x_vec - mu_c0) + 
+    log(c0_prior)
+}
+
+d1_bayes <- function(x_vec){
+  -.5 * log(norm(c1_sigma, type = "2")) - 
+    .5 * t(x_vec - mu_c1) %*% solve(c1_sigma) %*% (x_vec - mu_c1) + 
+    log(c1_prior)
+}
+
+d2_bayes <- function(x_vec){
+  -.5 * log(norm(c2_sigma, type = "2")) - 
+    .5 * t(x_vec - mu_c2) %*% solve(c2_sigma) %*% (x_vec - mu_c2) + 
+    log(c2_prior)
+}
+
+bayes_classifier <- function(x_vec){
+  score_c0 <- d0_bayes(x_vec)
+  score_c1 <- d1_bayes(x_vec)
+  score_c2 <- d2_bayes(x_vec)
+  
+  if (score_c0 > score_c1 & score_c0 > score_c2) {
+    0
+  } else if (score_c1 > score_c0 & score_c1 > score_c2) {
+    1
+  } else {
+    2
+  }
+}
+
+# Apply the Bayes classifier to the training data
+
+train_sample_df$bayes_predicted_y <- apply(train_sample_df[, c("X1", "X2")], 
+                                           1, bayes_classifier)
+
+#####
+# Step 3: Create the LDA Classifier and Apply To Training Set
+
+# The LDA classifier works the same way as the Bayes classifier, except we use
+# the sample means and covariance matrix and not the population means and
+# covariance matrix.
+
+sample_c0_mean_vec <- c(mean(train_sample_df$X1[train_sample_df$y == 0]), 
+                        mean(train_sample_df$X2[train_sample_df$y == 0]))
+sample_c1_mean_vec <- c(mean(train_sample_df$X1[train_sample_df$y == 1]), 
+                        mean(train_sample_df$X2[train_sample_df$y == 1]))
+sample_c2_mean_vec <- c(mean(train_sample_df$X1[train_sample_df$y == 2]), 
+                        mean(train_sample_df$X2[train_sample_df$y == 2]))
+
+# In reality, the sampled prior probabilities may not be the same as the
+# population prior probabilities. Here they are, so we do not technically need
+# to calculate the prior probabilities again but we will for completeness.
+
+c0_prior <- nrow(train_sample_df[train_sample_df$y == 0,]) /
+  nrow(train_sample_df)
+c1_prior <- nrow(train_sample_df[train_sample_df$y == 1,]) /
+  nrow(train_sample_df)
+c2_prior <- nrow(train_sample_df[train_sample_df$y == 2,]) /
+  nrow(train_sample_df)
+
+# Create and apply the LDA classifier, then plot the LDA decision boundaries
+
+sample_cov_mat_c0 <- cov(train_sample_df %>% 
+                           filter(y == 0) %>% 
+                           dplyr::select(X1, X2))
+
+sample_cov_mat_c1 <- cov(train_sample_df %>% 
+                           filter(y == 0) %>% 
+                           dplyr::select(X1, X2))
+
+sample_cov_mat_c2 <- cov(train_sample_df %>% 
+                           filter(y == 0) %>% 
+                           dplyr::select(X1, X2))
+
+# Unlike with LDA, we do not need to build a class-weighted covariance matrix
+# since we are not assuming that the class-level covariance matrices are equal
+
+# Create the LDA classifier
+
+d0_LDA <- function(x_vec){
+  -.5 * log(norm(sample_cov_mat_c0, type = "2")) - 
+    .5 * t(x_vec - sample_c0_mean_vec) %*% solve(sample_cov_mat_c0) %*% 
+    (x_vec - sample_c0_mean_vec) + 
+    log(c0_prior)
+}
+
+d1_LDA <- function(x_vec){
+  -.5 * log(norm(sample_cov_mat_c1, type = "2")) - 
+    .5 * t(x_vec - sample_c1_mean_vec) %*% solve(sample_cov_mat_c1) %*% 
+    (x_vec - sample_c1_mean_vec) + 
+    log(c1_prior)
+}
+
+d2_LDA <- function(x_vec){
+  -.5 * log(norm(sample_cov_mat_c2, type = "2")) - 
+    .5 * t(x_vec - sample_c2_mean_vec) %*% solve(sample_cov_mat_c2) %*% 
+    (x_vec - sample_c2_mean_vec) + 
+    log(c2_prior)
+}
+
+LDA_classifier <- function(x_vec){
+  score_c0 <- d0_LDA(x_vec)
+  score_c1 <- d1_LDA(x_vec)
+  score_c2 <- d2_LDA(x_vec)
+  
+  if (score_c0 > score_c1 & score_c0 > score_c2) {
+    0
+  } else if (score_c1 > score_c0 & score_c1 > score_c2) {
+    1
+  } else {
+    2
+  }
+}
+
+# Apply the Bayes classifier to the training data
+
+train_sample_df$LDA_predicted_y <- apply(train_sample_df[, c("X1", "X2")], 
+                                         1, LDA_classifier)
+
+#####
+# Step 4: Analyze performance
+
+# Apply our trained Bayes and LSA models to the test data
+
+test_sample_df$bayes_predicted_y <- apply(test_sample_df[, c("X1", "X2")], 
+                                          1, bayes_classifier)
+
+test_sample_df$LDA_predicted_y <- apply(test_sample_df[, c("X1", "X2")], 
+                                        1, LDA_classifier)
+
+# Find the misclassification rate of LDA in this case
+
+LDA_misclass_rate <- nrow(test_sample_df[test_sample_df$y != 
+                                           test_sample_df$LDA_predicted_y,]) /
+  nrow(test_sample_df)
+
+LDA_misclass_rate * 100
+
+# Again, not perfect! What does the Bayes classification error look like?
+
+bayes_misclass_rate <- nrow(test_sample_df[test_sample_df$y != 
+                                             test_sample_df$bayes_predicted_y,]) /
+  nrow(test_sample_df)
+
+bayes_misclass_rate * 100
+
+# LDA performs well - how close is it to the lowest possible misclassification
+# error, as provided by bayes?
+
+(LDA_misclass_rate - bayes_misclass_rate) * 100
---
+++ Logistic/univariate_binary_logistic.R
@@ -0,0 +1,231 @@
+# Logistic regression with a binary response and one predictor
+
+# Load packages and read data
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse)
+
+#####
+# Step 1: Data generation
+
+# Generate sample data for two outcomes (0 and 1). We will draw our x values
+# from two normal distributions with slightly different means to guarantee
+# some overlap between those observations with y = 0 and y = 1
+
+y <- append(rep(0, each = 500), 
+            rep(1, each = 500))
+
+means <- sample.int(5, 2, replace = FALSE)
+
+train_x <- append(rnorm(500, mean = means[1], sd = 1), 
+                  rnorm(500, mean = means[2], sd = 1))
+
+test_x <- append(rnorm(500, mean = means[1], sd = 1), 
+                 rnorm(500, mean = means[2], sd = 1))
+
+# We need to include an intercept term, so we will augment x with a 1-vector to
+# accommodate the intercept. This will be our design matrix, or essentially just
+# a one-power Vandermonde matrix
+
+train_x <- cbind(1, train_x)
+test_x <- cbind(1, test_x)
+
+#####
+# Step 2: Visualize the log-likelihood surface
+
+# Logistic regression, like regular regression, is simply an optimization
+# problem, except in this case the function we are maximizing is the log-
+# likelihood function
+#
+# l(b) = sum(y %*% t(b) %*% x - log(1 + exp(t(b) %*% x)))
+#
+# which is strictly concave, so it will have one maximum. We will have
+# two beta coefficients (one for the intercept and one for our single
+# independent variable) so we can create a contour plot of the log-likelihood
+# function's surface across values of these two coefficients.
+
+b0_vec <- seq(-5, 5, .25)
+b1_vec <- seq(-5, 5, .25)
+
+surface <- expand_grid(b0_vec, b1_vec)
+train_data <- cbind(y, train_x)
+
+log_like_func <- function(data, b_0, b_1){
+  
+  b_vec <- c(b_0, b_1)
+  
+  data[1] %*% t(b_vec) %*% data[2:3] -
+    log(1 + exp(t(b_vec) %*% data[2:3]))
+
+}
+
+log_like <- function(b_vec){
+  
+  sum(apply(train_data, 1, log_like_func, b_0 = b_vec[1], b_1 = b_vec[2]))
+  
+}
+
+surface$l <- apply(surface, 1, log_like)
+
+likelihood_surface_plot <- ggplot(surface, aes(b0_vec, b1_vec, z = l)) +
+  geom_contour_filled(bins = 20) +
+  labs(title = "Surface Plot of Log-Likelihood Function",
+       subtitle = "Evaluated Across Range of Beta Coefficients",
+       x = "Intercept",
+       y = "B1",
+       fill = "Log-Likelihood")
+
+#####
+# Step 2a: Newton-Raphson/iteratively re-weighted least squares algorithm
+
+# This is the method described in Elements of Statistical Learning
+
+# To calculate the regression coefficients for our logistic regression we need
+# to chose the coefficients that maximize the log-likelihood function
+#
+# l(b) = sum(y %*% t(b) %*% x - log(1 + exp(t(b) %*% x)))
+#
+# and in order to do this we obviously take its derivative and set it equal to
+# zero. This derivative is the score equation
+#
+# dl(b)/db = sum(x %*% (y - p)) = 0
+#
+# where p is the fitted probabilities for each observation given our betas. We
+# will use iteratively re-weighted least squares (see ESL) to "update" our beta
+# coefficients over a number of iterations until our score equation evaluates 
+# within a tolerable distance from zero (which signifies that we have maximized 
+# the log-likelihood well enough).
+
+beta_vec_newton <- c(0,0)
+max_check <- FALSE
+iter_newton <- 0
+
+while (max_check != TRUE) {
+  
+  p <- exp(train_x %*% beta_vec_newton) / 
+    (1 + exp(train_x %*% beta_vec_newton))
+
+  W <- diag(as.vector(p))
+  
+  z <- train_x %*% beta_vec_newton + solve(W) %*% (y - p)
+  
+  beta_vec_newton <- solve(t(train_x) %*% W %*% train_x) %*% 
+    t(train_x) %*% W %*% z
+  
+  # Check that we are actually maximizing the likelihood function by checking
+  # if the score function is evaluating to zero
+  
+  score <- t(train_x) %*% (y - p)
+  score <- c(score[1], score[2])
+  max_check <- all.equal(c(0,0), score, 
+                         tolerance = .Machine$double.eps ^ 0.5)
+  iter_newton <- iter_newton + 1
+  
+}
+
+#####
+# Step 2b: Modified algorithm
+
+# W is a bit nasty to work with, so we can also use a modified algorithm that
+# incorporates W more directly. See 
+# http://personal.psu.edu/jol2/course/stat597e/notes2/logit.pdf.
+
+beta_vec_mod <- c(0,0)
+max_check <- FALSE
+iter_mod <- 0
+
+while (max_check != TRUE) {
+  
+  p <- exp(train_x %*% beta_vec_mod) / 
+    (1 + exp(train_x %*% beta_vec_mod))
+  
+  # Use `sweep` to multiply each row of x by scalar vector
+  
+  x_tilde <- sweep(train_x, MARGIN = 2, p * (1 - p), `*`)
+  
+  beta_vec_mod <- beta_vec_mod + solve(t(train_x) %*% x_tilde) %*%
+    t(train_x) %*%
+    (y - p)
+  
+  # Check that we are actually maximizing the likelihood function by checking
+  # if the score function is evaluating to zero
+  
+  score <- t(train_x) %*% (y - p)
+  score <- c(score[1], score[2])
+  max_check <- all.equal(c(0,0), score, 
+                         tolerance = .Machine$double.eps ^ 0.5)
+  iter_mod <- iter_mod + 1
+  
+}
+
+# The modified algorithm converges much more quickly than Newton-Raphson/IRLS:
+
+iter_newton
+iter_mod
+
+# Does it appear as though these coefficients maximize the log-likelihood?
+
+likelihood_surface_plot <- likelihood_surface_plot +
+  geom_point(aes(x = beta_vec_mod[1], y = beta_vec_mod[2]))
+
+likelihood_surface_plot
+
+# Let's check our coefficients against those found by R's `glm` function
+
+glm_model <- glm(y ~ train_x, data = train_data, family = "binomial")
+
+glm_model$coefficients
+beta_vec_newton
+beta_vec_mod
+
+#####
+# 3: Visualize fit and test
+
+# To plot the logistic fit we will use our coefficients to calculate the
+# value predicted by the logistic coefficients and then plot those predictions
+# against the actual values. For ease of plotting we will also convert the data
+# matrix into a tibble, though this isn't strictly necessary.
+
+train_data <- as_tibble(train_data)
+colnames(train_data) <- c("y", "int", "x")
+
+train_data$pred_prob <- exp(beta_vec_mod[1] + train_data$x * beta_vec_mod[2]) /
+  (1 + exp(beta_vec_mod[1] + train_data$x * beta_vec_mod[2]))
+
+logistic_plot <- ggplot(train_data) +
+  geom_point(aes(x, y)) +
+  geom_line(aes(x, pred_prob))
+
+logistic_plot
+
+# Let's also calculate the training error and include in the logistic fit plot
+# which class our model assigns to the points
+
+train_data$pred <- if_else(train_data$pred_prob < .5, 0, 1)
+
+(nrow(train_data[(train_data$y != train_data$pred),]) / nrow(train_data)) * 100
+
+logistic_plot_preds <- ggplot(train_data) +
+  geom_point(aes(x, y, color = as.factor(pred))) +
+  geom_line(aes(x, pred_prob)) +
+  scale_color_manual(name = "Predicted y",
+                      values = c("red", "blue"))
+
+logistic_plot_preds
+
+# To test the model, we'll use our coefficients from the model fit to the
+# training data to predict outcome classifications for the test data: first we
+# will calculate pr(Y = 1| X = x) for each observation using the training fit, 
+# then assign Y = 0 if the predicted probability is less than .5 and Y = 1 if it 
+# is greater than .5
+
+test_data <- as_tibble(cbind(y, test_x))
+colnames(test_data) <- c("y", "int", "x")
+
+test_data$pred_prob <- exp(beta_vec_mod[1] + test_data$x * beta_vec_mod[2]) /
+  (1 + exp(beta_vec_mod[1] + test_data$x * beta_vec_mod[2]))
+
+test_data$pred <- if_else(test_data$pred_prob < .5, 0, 1)
+
+# Calculate the test error
+
+(nrow(test_data[(test_data$y != test_data$pred),]) / nrow(test_data)) * 100
